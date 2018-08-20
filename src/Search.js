8acd06215b0a72109ee0322095d206144a861bb5
Natalia Braz <naabraz@gmail.com>
2018-08-20 13:31:04 -0300

    Calling search method
    Adjustment to call search method

--- src/Search.js
+++ src/Search.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
+import * as BooksAPI from './BooksAPI'
+
 class Search extends Component {
+
+  booksResult = []
+
+  searchBook(query) {
+    BooksAPI.search(query).then((response) => {
+      this.booksResult = response
+        console.log(this.booksResult)
+    })
+  }
 
   render() {
     return (
@@ -9,18 +20,11 @@ class Search extends Component {
         <div className="search-books-bar">
           <Link className="close-search" to="/">Close</Link>
           <div className="search-books-input-wrapper">
-            {/*
-              NOTES: The search from BooksAPI is limited to a particular set of search terms.
-              You can find these search terms here:
-              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-              you don't find a specific author or title. Every search is limited by search terms.
-            */}
-            <input type="text" placeholder="Search by title or author"/>
+            <input type="text" placeholder="Search by title or author" onChange={(event) => this.searchBook(event.target.value)}/>
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+          {this.booksResult.map((book) => <ol className="books-grid">{book.title}</ol>)}
         </div>
       </div>
     )
