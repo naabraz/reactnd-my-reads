dd93eaf60c831c27bb6972227af15e2c6d4ef34b
Natalia Braz <naabraz@gmail.com>
2018-08-23 14:22:14 -0300

    Removing
    Removing unused file

--- src/service/BookService.js
+++
@@ -1,7 +0,0 @@
-import * as BooksAPI from './BooksAPI'
-
-const changeShelfBook = (shelf, book) => {
-  BooksAPI.update(book, shelf).then(() => this.componentDidMount())
-}
-
-module.exports = { changeShelfBook }
