644b103f3f108eff833b363064062518582cc950
Natalia Braz <naabraz@gmail.com>
2018-11-26 17:47:48 -0200

    Adding Unit Testing
    Adding unit testing to Book component

---
+++ src/components/tests/Book.test.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import sinon from 'sinon'
+import { mount } from './enzyme'
+
+import Book from '../Book'
+
+describe('Book component test', () => {
+  let wrapper
+  let book = {id: '123', title: 'Star Wars', imageLinks: {thumbnail: 'img.com'}, authors: ['George Lucas']}
+
+  beforeEach(() => {
+    const changeShelfBook = () => sinon.stub()
+    const addShelf = () => sinon.stub()
+
+    wrapper = mount(<Book book={book} changeShelfBook={changeShelfBook} addShelf={addShelf} />)
+  })
+
+  it('renders Book component', () => {
+    expect(wrapper.find('.book')).toBeDefined()
+  })
+
+  it('renders Book thumbnail', () => {
+    const thumbnail = <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
+
+    expect(wrapper.contains(thumbnail)).toBeTruthy()
+  })
+})
--- src/components/tests/ListShelves.test.js
+++ src/components/tests/ListShelves.test.js
@@ -29,5 +29,4 @@ describe('List Shelves component test', () => {
 
     expect(wrapper.contains(title)).toBeTruthy()
   })
-
 })
