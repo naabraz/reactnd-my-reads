83de2acbd99c355e301f47bd9c138afe9fd350d2
Natalia Braz <naabraz@gmail.com>
2018-11-28 00:15:18 -0200

    Adding test to search book method. Needs adjust

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import {DebounceInput} from 'react-debounce-input';
+import { DebounceInput } from 'react-debounce-input'
 
 import BookFormatter from '../helpers/BookFormatter'
 import ShelfHelper from '../helpers/ShelfHelper'
--- src/components/tests/Search.test.js
+++ src/components/tests/Search.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import sinon from 'sinon'
 import { mount } from './enzyme'
 import { Route, BrowserRouter, Link } from 'react-router-dom'
+import { DebounceInput } from 'react-debounce-input'
 
 import Search from '../Search'
 
@@ -19,11 +20,22 @@ describe('Search component test', () => {
     )
   })
 
-  afterEach(() => {
-    wrapper.unmount()
+  it('should render Search component', () => {
+    expect(wrapper.find('div.search-books').length).toBe(1)
   })
 
-  it('renders Search component', () => {
-    expect(wrapper.find('div.search-books').length).toBe(1)
+  it('should render Link component to close search', () => {
+    expect(wrapper.find(Link).length).toBe(1)
+    expect(wrapper.find('[to="/"]').length).toBe(1)
+  })
+
+  it('should search books', () => {
+    const spy = jest.spyOn(Search.prototype, 'searchBooks')
+
+    wrapper.setState({query: ''})
+    expect(wrapper.find(DebounceInput).simulate('change', { target: { value: 'Star Wars' }}))
+    //expect(wrapper.state('query')).toEqual('Star Wars')
+    spy('Star Wars')
+    expect(spy).toBeCalledWith('Star Wars')
   })
 })
