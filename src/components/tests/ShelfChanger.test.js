e537476de2811a798d8f377ab24048165b5f372d
Natalia Braz <naabraz@gmail.com>
2018-12-04 13:06:42 -0200

    Adding ShelfChanger test

--- src/components/tests/ShelfChanger.test.js
+++ src/components/tests/ShelfChanger.test.js
@@ -7,8 +7,8 @@ import ShelfChanger from '../ShelfChanger'
 describe('List Shelves component test', () => {
   let wrapper
   let book = {id: '123', imageLinks: {thumbnail: 'img.com'}, shelf: 'Want To Read', title: 'Star Wars', authors: ['George Lucas']}
-  const changeShelfBook = () => sinon.stub()
-  const addToShelf = () => sinon.stub()
+  const changeShelfBook = sinon.stub()
+  const addToShelf = sinon.stub()
 
   beforeEach(() => {
     Object.defineProperty(window.location, 'pathname', {
@@ -51,6 +51,28 @@ describe('List Shelves component test', () => {
     wrapper = shallow(<ShelfChanger book={book} addToShelf={addToShelf} changeShelfBook={changeShelfBook} />)
 
     expect(wrapper.find('select[value="none"]')).toHaveLength(1)
+  })
+
+  it('should add a book to shelf', () => {
+    const event = { target: { value: 'Read' } }
+
+    wrapper.find('select').simulate('change', event)
+    
+    expect(addToShelf.called).toEqual(true)
+  })
+
+  it('should change a book shelf', () => {
+    Object.defineProperty(window.location, 'pathname', {
+      writable: true,
+      value: '/'
+    })
+
+    const event = { target: { value: 'Want To Read' } }
+    wrapper = shallow(<ShelfChanger book={book} addToShelf={addToShelf} changeShelfBook={changeShelfBook} />)
+
+    wrapper.find('select').simulate('change', event)
+    
+    expect(changeShelfBook.called).toEqual(true)
   })
 
 })
